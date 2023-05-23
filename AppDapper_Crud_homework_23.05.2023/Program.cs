f8cd60436cc638fad9a9ffbcc608d930d2564b1a
Shahrom_developer <shahrom909050409@gmail.com>
2023-05-23 18:15:01 +0500

    first commit

---
+++ AppDapper_Crud_homework_23.05.2023/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+
+using Domain.Dtos;
+using Infrastructure.Services;
+
+var studentSerice = new StudentService();
+//Show();
+void Show()
+{
+    var students = studentSerice.GetStudents();
+    Console.WriteLine("Id-------FirstName----------LastName------Age------email");
+    foreach (var student in students)
+    {
+        Console.WriteLine($"{student.Id}-------{student.FirstName}----------" +
+            $" {student.LastName}------{student.Age}------ {student.email}");
+    }
+     
+}
+
+//GetById(7);
+//void GetById(int id)
+//{
+//    var student = studentSerice.GetById(id);
+//    Console.WriteLine("GetById");
+//    Console.WriteLine($" Id: {student.Id}\n" +
+//        $" FirstName: {student.FirstName} \n" +
+//        $" LastName: {student.LastName} \n" +
+//        $" Age:{student.Age} \n" +
+//        $" Email: {student.email}");  
+//}
+
+//var student = new StudentDto()
+//{
+//    FirstName = "Shahzod",
+//    LastName = "Shahzod",
+//    Age = 20,
+//    email = "Shahzod@gmail.com",
+//};
+//GetIdAddStudent(student); 
+
+//void GetIdAddStudent(StudentDto studentDto)
+//{
+//    var result = studentSerice.GetIdAddStudent(studentDto);
+//    Console.WriteLine(result.Id);
+//}
+
+
+//var s = new StudentDto()
+//{
+//    Id = 9,
+//    FirstName = "Test",
+//    LastName = "Test",
+//    Age = 30,
+//    email = "test@gmail.com"
+
+//};
+//Update(s);
+//void Update(StudentDto studentDto)
+//{
+//    var student = studentSerice.UpdateStudent(studentDto);
+//    Console.WriteLine(student);
+
+//}
+
+//DeleteStudent(11);
+
+void DeleteStudent(int id)
+{
+    var student = studentSerice.DeleteStudent(id);
+    Console.WriteLine(student);
+
+}
---
+++ Infrastructure/Services/StudentService.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using Domain.Dtos;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class StudentService
+    {
+        string connectionString = "Server=localhost; Port=5432; Database=Dapper2; User Id=postgres; Password=233223;";
+        public StudentService()
+        {
+            
+        }
+        public List<StudentDto>GetStudents()
+        {
+            using(var conn = new NpgsqlConnection(connectionString))
+            {
+                var sql = $"select id, first_name as FirstName, last_name as LastName," +
+                    $" Age, email from Students";
+                var result = conn.Query<StudentDto>(sql);  return result.ToList(); 
+            } 
+        }
+        //GetById
+        public StudentDto GetById(int id)
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                var sql = $"select id, first_name as FirstName, last_name as LastName, Age, email from Students " +
+                    $"where id = {id}";
+                var result = conn.QuerySingle<StudentDto>(sql);
+                return result; 
+            }
+        }
+        // insert
+        public StudentDto GetIdAddStudent(StudentDto student)  
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                var sql = $"insert into Students(first_name, last_name, Age, email)values(@FirstName,@FirstName, @Age, @email)returning id;";
+                var result = conn.ExecuteScalar<int>(sql,student);
+                student.Id = result;
+                return student;   
+            }
+        }
+        // Update   
+        public int UpdateStudent(StudentDto student)
+        {
+            using(var conn = new NpgsqlConnection(connectionString))
+            {
+                var sql = $"update students set " +
+                $"first_name = '{student.FirstName}'," +
+                $"last_name = '{student.LastName}'," +
+                $"Age = {student.Age}, " +
+                $"email = '{student.email}'" +
+                $"where id = {student.Id}";
+                var result = conn.Execute(sql);      
+                return result;
+            }
+        }
+
+        public int DeleteStudent(int id)
+        {
+            using(var conn = new NpgsqlConnection(connectionString))
+            {
+                var sql = $"delete from Students where id = {id}";
+                var result = conn.Execute(sql);
+                return result; 
+            }
+        }
+    }
+}
